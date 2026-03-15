8324b4d15e3ea3a98b93da1d204d2c72a1659b9d
minakent123 <minakent123@gmail.com>
2026-03-15 23:40:03 +0900

    Add line drawing

--- src/core/SoftwareRenderer.cpp
+++ src/core/SoftwareRenderer.cpp
@@ -13,6 +13,40 @@ uint32_t PackRgba8(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
            (static_cast<uint32_t>(a) << 24U);
 }
 
+int Abs(int value)
+{
+    return value < 0 ? -value : value;
+}
+
+void DrawLine(Surface& target, int x0, int y0, int x1, int y1, uint32_t color)
+{
+    const int deltaX = Abs(x1 - x0);
+    const int deltaY = Abs(y1 - y0);
+    const int stepX = x0 < x1 ? 1 : -1;
+    const int stepY = y0 < y1 ? 1 : -1;
+
+    int error = deltaX - deltaY;
+
+    while (true) {
+        target.PutPixel(x0, y0, color);
+
+        if (x0 == x1 && y0 == y1) {
+            break;
+        }
+
+        const int doubledError = error * 2;
+        if (doubledError > -deltaY) {
+            error -= deltaY;
+            x0 += stepX;
+        }
+
+        if (doubledError < deltaX) {
+            error += deltaX;
+            y0 += stepY;
+        }
+    }
+}
+
 }  // namespace
 
 void SoftwareRenderer::Render(Surface& target, double /*timeSeconds*/) const
@@ -30,11 +64,12 @@ void SoftwareRenderer::Render(Surface& target, double /*timeSeconds*/) const
     const int centerY = height / 2;
     const int lineLength = width / 2;
     const int lineStartX = (width - lineLength) / 2;
-    const int lineEndX = lineStartX + lineLength;
+    const int lineEndX = lineStartX + lineLength - 1;
+    const int diagonalStartY = height / 3;
+    const int diagonalEndY = diagonalStartY + (height / 3);
 
-    for (int x = lineStartX; x < lineEndX; ++x) {
-        target.PutPixel(x, centerY, PackRgba8(255, 255, 255, 255));
-    }
+    DrawLine(target, lineStartX, centerY, lineEndX, centerY, PackRgba8(255, 255, 255, 255));
+    DrawLine(target, lineStartX, diagonalStartY, lineEndX, diagonalEndY, PackRgba8(255, 180, 80, 255));
 }
 
 }  // namespace core
